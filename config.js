eed94df7cc20c3eab5e8766c4bd5c0d87bf0baf7
Sheet-Tee <cjdirocco@yahoo.com>
2026-03-18 14:27:32 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  clubId: "strawberry",
+  clubName: "Strawberry Creek",
+  tagline: "Private Golf & Social Club",
+  logoUrl: "./assets/logo.png",
+  groupPassword: "golf", 
+  supabaseUrl: "https://nwizmbkurxxqcfzmrzrx.supabase.co",
+  supabaseKey: "PASTE_YOUR_ACTUAL_STRAWBERRY_ANON_KEY_HERE"
+};
